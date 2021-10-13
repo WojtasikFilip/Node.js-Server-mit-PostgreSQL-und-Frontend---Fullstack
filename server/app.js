58a541beadc64a14ebbe1481574b3c7c2ca9f160
Filip <wojtasik.f03@htlwienwest.at>
2021-10-13 08:43:58 +0200

    Daten werden angezeigt

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const path = require('path');
+const cors = require('cors');
 const routes = require('./routes/cars.js');
 const { errorHandler, notFound } = require('./middleware/errorHandler');
 require('colors');
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 
